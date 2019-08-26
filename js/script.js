c55b55010b0508cc6687b7fdd946d81d151bb22c
Jecinta wanjiru <jecintawanjirug@gmail.com>
2019-08-26 09:05:14 +0300

    this resizes the portfolio images

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$("document").ready(function(){
+$(".design-image").click(function(){
+    $(".design").show();
+    $(".design-image").hide();
+});
+$(".dev-image").click(function(){
+    $(".development").show();
+    $(".dev-image").hide();
+});
+$(".prod-image").click(function(){
+    $(".product").show();
+    $(".prod-image").hide();
+});
+
+$(".design").click(function(){
+    $(".design-image").show();
+    $(".design").hide();
+});
+$(".development").click(function(){
+    $(".dev-image").show();
+    $(".development").hide();
+});
+$(".product").click(function(){
+    $(".prod-image").show();
+    $(".product").hide();
+});
+
+$("#button").click(function(){
+    event.preventDefault();
+})
+
+
+
+
+
+  
+
+
+
+
+});   
